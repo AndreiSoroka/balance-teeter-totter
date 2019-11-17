2d4b84949d83849db6d12d75c08e1bce3256fc0e
andreisoroka <unickpro@gmail.com>
2019-11-17 13:25:38 +0200

    [App][Playground] created layout

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,66 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>
+      <h2>Balance teeter-totter</h2>
+    </header>
+    <div class="wrapper">
+      <Playground/>
+    </div>
+    <footer>
+      <div class="wrapper">
+        Github:
+        <a href="https://github.com/AndreiSoroka/balance-teeter-totter"
+           target="_blank"
+           rel="nofollow noopener">
+          AndreiSoroka/balance-teeter-totter
+        </a>
+      </div>
+    </footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import Playground from './components/Playground.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld,
+    Playground,
   },
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import "~normalize.css";
+
+  body {
+    background: #efefef;
+    min-width: 720px;
+  }
+
+  a {
+    color: #42b983;
+  }
+
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+  }
+
+  .wrapper {
+    max-width: 700px;
+    margin: 0 auto;
+  }
+
+  header {
+    text-align: center;
+    padding: 10px;
+  }
+
+  footer {
+    margin: 2rem 0 0;
+    padding: 10px;
+  }
 </style>
---
+++ src/components/Playground.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="playground">
+    <div class="battle-ground"></div>
+    <div class="libra-arm">
+      test
+    </div>
+    <div class="libra-base">
+      <div class="libra-base__description">test</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Playground',
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<style scoped lang="scss">
+  .playground {
+    margin: 1rem 0;
+  }
+
+  .battle-ground {
+    color: red;
+    height: 300px;
+  }
+
+  .libra-arm {
+
+    &::after {
+      content: "";
+      display: block;
+      height: 10px;
+      background: #2c3e50;
+    }
+  }
+
+  .libra-base {
+    width: 0;
+    height: 0;
+    border: 50px solid transparent;
+    border-bottom: 100px solid #2c3e50;
+    margin: -50px auto 0;
+
+    .libra-base__description {
+      width: 100px;
+      margin: 75px 0 0 -50px;
+      color: white;
+      text-align: center;
+    }
+  }
+</style>
